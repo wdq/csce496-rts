3ecb326d384fea1e53c104a8a514470375203a4c
William Quade <liam@quade.co>
2018-04-12 09:26:44 -0500

    This doesn't immediately crash which is good. It does get stuck though, I think  the gyro values aren't working like they should.

--- lab01/wquade-lab01/wquade-lab01.ino
+++ lab01/wquade-lab01/wquade-lab01.ino
@@ -1,8 +1,59 @@
 #include <Arduino_FreeRTOS.h>
-#include "RingoHardware.h"
+#include <Adafruit_NeoPixel.h>
+#include "ci2c.h"
 
 #define TURN_ANGLE    88 // 87 sometimes seems to work better, other times 88
 #define CYCLES_SINCE_CORRECTION_THRESHOLD  100
+
+// For LED Pixels
+#define Light_Bus_BTN1 7 //for 6 neo pixel RGB
+
+#define NUM_PIXELS 6
+
+#define EYE_LEFT 5
+#define EYE_RIGHT 4 
+#define BODY_TOP 3
+#define BODY_BOTTOM 2
+#define TAIL_TOP 0
+#define TAIL_BOTTOM 1
+
+// For motors
+#define MotorDirection_Right 1
+#define MotorDirection_Left 0
+#define MotorDrive_Left 6
+#define MotorDrive_Right 5
+
+// For gyro
+#define    L3GD20_REGISTER_WHO_AM_I             0x0F   // 11010100   r
+#define   L3GD20_REGISTER_CTRL_REG1            0x20   // 00000111   rw
+#define   L3GD20_REGISTER_CTRL_REG2            0x21   // 00000000   rw
+#define   L3GD20_REGISTER_CTRL_REG3            0x22   // 00000000   rw
+#define   L3GD20_REGISTER_CTRL_REG4            0x23   // 00000000   rw
+#define   L3GD20_REGISTER_CTRL_REG5            0x24   // 00000000   rw
+#define   L3GD20_REGISTER_REFERENCE            0x25   // 00000000   rw
+#define   L3GD20_REGISTER_OUT_TEMP             0x26   //            r
+#define   L3GD20_REGISTER_STATUS_REG           0x27   //            r
+#define   L3GD20_REGISTER_OUT_X_L              0x28   //            r
+#define   L3GD20_REGISTER_OUT_X_H              0x29   //            r
+#define   L3GD20_REGISTER_OUT_Y_L              0x2A   //            r
+#define   L3GD20_REGISTER_OUT_Y_H              0x2B   //            r
+#define   L3GD20_REGISTER_OUT_Z_L              0x2C   //            r
+#define   L3GD20_REGISTER_OUT_Z_H              0x2D   //            r
+#define   L3GD20_REGISTER_FIFO_CTRL_REG        0x2E   // 00000000   rw
+#define   L3GD20_REGISTER_FIFO_SRC_REG         0x2F   //            r
+#define   L3GD20_REGISTER_INT1_CFG             0x30   // 00000000   rw
+#define   L3GD20_REGISTER_INT1_SRC             0x31   //            r
+#define   L3GD20_REGISTER_TSH_XH               0x32   // 00000000   rw
+#define   L3GD20_REGISTER_TSH_XL               0x33   // 00000000   rw
+#define   L3GD20_REGISTER_TSH_YH               0x34   // 00000000   rw
+#define   L3GD20_REGISTER_TSH_YL               0x35   // 00000000   rw
+#define   L3GD20_REGISTER_TSH_ZH               0x36   // 00000000   rw
+#define   L3GD20_REGISTER_TSH_ZL               0x37   // 00000000   rw
+#define   L3GD20_REGISTER_INT1_DURATION        0x38   // 00000000   rw
+
+#define L3GD20_SENSITIVITY_250DPS  (0.00875F)      // Roughly 22/256 for fixed point match
+
+#define   GYRO_ADDRESS  0x6B
 
 // Some global variables
 bool isTurning = false; // These first two are used to keep track of if we're going straight or turning, defaults to straight.
@@ -13,6 +64,13 @@ bool isObstacle = false; // Keep track of if there is an obstacle in the way
 uint8_t cyclesSinceCorrectionLeft = 0;
 uint8_t cyclesSinceCorrectionRight = 0;
 uint8_t cyclesSinceCorrectionStraight = 0;
+
+Adafruit_NeoPixel myPixels = Adafruit_NeoPixel(NUM_PIXELS, Light_Bus_BTN1, NEO_GRB + NEO_KHZ800); // for LEDs
+
+float gyroAngle = 0; // For gyro
+int16_t gyroDegrees = 0;
+uint8_t gyroLoopCount = 0;
+I2C_SLAVE gyro; // Gyroscope I2C object
 
 // Struct to hold PID controller parameters.
 struct PID {
@@ -37,11 +95,103 @@ uint8_t directionDataDistance;
 directionData directions[8];
 
 
-
 // Define tasks
 void TaskTurn(void *pvParameters); // Turn
 void TaskDriveStraight(void *pvParameters); // Go straight
 void TaskControl(void *pvParameters);   // Control the robot by triggering the other tasks.
+void TaskGyro(void *pvParameters); // Update gyro degrees
+
+// Helper functions
+
+// Initialize pixels
+void SetupPixels() {
+  // Use lights
+  pinMode(Light_Bus_BTN1,OUTPUT);
+  digitalWrite(Light_Bus_BTN1, LOW); 
+}
+
+// Set pixel color
+void SetPixelRGB(int Pixel, int Red, int Green, int Blue){
+  //while(xSemaphoreTake(ledSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+  myPixels.setPixelColor(Pixel, myPixels.Color(Red,Green,Blue));
+  //xSemaphoreGive(ledSemaphore); // Release the semaphore 
+  
+}
+
+// Refresh pixel colors to set ones
+void RefreshPixels(){
+  //while(xSemaphoreTake(ledSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+  myPixels.show();  
+  //xSemaphoreGive(ledSemaphore); // Release the semaphore 
+}
+
+// Initialize motors
+void SetupMotors() {
+  // Motors begin
+  pinMode(MotorDirection_Left, OUTPUT);
+  pinMode(MotorDirection_Right, OUTPUT);
+  analogWrite(MotorDrive_Left,0);
+  analogWrite(MotorDrive_Right,0); 
+}
+
+// Drive motors
+void Motors(int16_t LeftMotor, int16_t RightMotor) {
+  //while(xSemaphoreTake(motorSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+  // Had some logic to bound the speeds to the min/max.
+  // I removed this since not needed since I know the bounds.
+  // Also got rid of the use of the global motor speed variables.
+  if(LeftMotor<0){
+    digitalWrite(MotorDirection_Left,0);
+  } else {
+    digitalWrite(MotorDirection_Left,1);
+  }
+  if(RightMotor<0){
+    digitalWrite(MotorDirection_Right,0);
+  } else {
+    digitalWrite(MotorDirection_Right,1);
+  }
+    
+  analogWrite(MotorDrive_Left,abs(LeftMotor));
+  analogWrite(MotorDrive_Right,abs(RightMotor));
+  //xSemaphoreGive(motorSemaphore); // Release the semaphore 
+}
+
+// Get the degrees from the gyro
+int16_t GetDegrees() {
+  //while(xSemaphoreTake(gyroSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+  //return (int16_t)(gyroAngle * 9.0); // This scale factor gets it to the angle in degrees in integer
+  return (int16_t)(gyroAngle);
+  //xSemaphoreGive(gyroSemaphore); // Release the semaphore 
+}
+
+// Read a byte over I2C, helper function
+uint8_t i2cRead8(uint8_t reg) {
+  uint8_t out[1];
+  I2C_read(&gyro, reg, &out[0], 1); // read 1 byte at reg into out
+  return out[0]; 
+}
+
+// Write a byte over I2C, helper function
+void i2cWrite8(uint8_t reg, uint8_t value) {
+  uint8_t in[1];
+  in[0] = value;
+  I2C_write(&gyro, reg, &in[0], 1); // write 1 byte from in into reg
+}
+
+// Initalize gyro
+void GyroSetup() {
+  I2C_init(I2C_FM); // init I2C bus
+
+  // Initialize the gyro
+  //while(xSemaphoreTake(gyroSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+  I2C_slave_init(&gyro, GYRO_ADDRESS, I2C_8B_REG);
+  uint8_t id = i2cRead8(L3GD20_REGISTER_WHO_AM_I); // get ID, for testing
+  i2cWrite8(L3GD20_REGISTER_CTRL_REG1, 0x0F); // Normal mode, enable all three channels
+  i2cWrite8(L3GD20_REGISTER_CTRL_REG4, 0x00); // Make sure it's 250 dps resolution 
+  //xSemaphoreGive(gyroSemaphore); // Release the semaphore 
+  //Serial.print("ID: ");
+  //Serial.println(id);  
+}
 
 // Check for obstacle based on PID controller corrections not moving, return true if obstacle, otherwise false.
 bool checkForObstacle() {
@@ -62,27 +212,23 @@ bool checkForObstacle() {
   return false; // No obstacle
 }
 
-// Setup ringo stuff
-void ringoSetup() {
-  HardwareBegin();        //initialize Ringo's brain to work with his circuitry   
-  //PlayStartChirp();       //Play startup chirp and blink eyes
-  //NavigationBegin();
-  //SimpleGyroNavigation(); 
-  SwitchMotorsToSerial(); //Call "SwitchMotorsToSerial()" before using Serial.print functions as motors & serial share a line
-  RestartTimer();  
-  NavigationBegin();  
-}
-
 // Setup the tasks
 void taskSetup() {
   // Create tasks   
+  xTaskCreate(
+    TaskGyro, // task function
+    (const portCHAR *)"Gyro", // task name string
+    95, // stack size
+    NULL, // nothing
+    0, // Priority, 0 is lowest
+    NULL); // nothing
        
   xTaskCreate(
     TaskTurn, // task function
     (const portCHAR *)"Turn", // task name string
     95, // stack size
     NULL, // nothing
-    0, // Priority, 0 is lowest
+    1, // Priority, 0 is lowest
     NULL); // nothing
     
   xTaskCreate(
@@ -90,7 +236,7 @@ void taskSetup() {
     (const portCHAR *)"DriveStraight", // task name string
     110, // stack size
     NULL, // nothing
-    1, // Priority, 0 is lowest
+    2, // Priority, 0 is lowest
     NULL); // nothing   
     
   xTaskCreate(
@@ -98,17 +244,19 @@ void taskSetup() {
     (const portCHAR *)"Control", // task name string
     145, // stack size
     NULL, // nothing
-    2, // Priority, 0 is lowest
+    3, // Priority, 0 is lowest
     NULL); // nothing                   
 }
 
 // First run code
 void setup(){  
   delay(2000); // Delay so that my hand can move away before gyro calibrates
-  ringoSetup(); // Setup ringo stuff
-  Serial.begin(9600); // For debugging
-  Serial.println("Setup");  
-    Serial.println("Starting tasks");
+  GyroSetup();
+  SetupPixels();
+  SetupMotors();
+  //Serial.begin(9600); // For debugging
+  //Serial.println("Setup");  
+  //Serial.println("Starting tasks");
     taskSetup(); // Setup the tasks
 }
 
@@ -128,10 +276,10 @@ void TaskTurn(void *pvParameters) {
       PID pid = (PID){.kp=3, .ki=0, .kd=100, .integral=0, .error=0, .dt=25, .minimum=-90, .maximum=90}; // setup the PID controller
       Motors(0,0); // Make sure the motors have stopped before doing anything (todo: maybe a small delay?)
       //ZeroNavigation();
-      SimpleGyroNavigation(); // Pull sensors
+      //SimpleGyroNavigation(); // Pull sensors
       int16_t setHeading = directionDataAngle;
       while(isTurning) { /* begin turning 90 degrees loop */
-        SimpleGyroNavigation(); // Pull sensors
+        //SimpleGyroNavigation(); // Pull sensors
         int16_t currentHeading = GetDegrees();
         if(abs(abs(setHeading) - abs(currentHeading)) == 0) { // If we have reached set point, stop.
           Motors(0,0);
@@ -171,10 +319,10 @@ void TaskDriveStraight(void *pvParameters) {
       PID pid = (PID){.kp=50, .ki=0, .kd=0, .integral=0, .error=0, .dt=50, .minimum=-100, .maximum=100}; // setup the PID controller      
       Motors(0,0); // Make sure the motors have stopped before doing anything (todo: maybe a small delay?)
       //ZeroNavigation();
-      SimpleGyroNavigation(); // Pull sensors
+      //SimpleGyroNavigation(); // Pull sensors
       int16_t setHeading = directionDataAngle;
       while(isDrivingStraight) { /* begin driving straight loop */
-        SimpleGyroNavigation();  // Pull sensors
+        //SimpleGyroNavigation();  // Pull sensors
         int16_t currentHeading = GetDegrees();
         int16_t output = CalculatePID(setHeading, currentHeading, &pid); // Get control output
         int16_t headingDiff = currentHeading - setHeading; // Figure out if we need to move left or right, and control motors based on that
@@ -239,7 +387,7 @@ void TaskControl(void *pvParameters) {
    // Task loop here
    int directionIndex = 0;
    while(1) { /* begin task loop */
-    //SimpleGyroNavigation(); 
+    ////SimpleGyroNavigation(); 
     //int16_t currentHeading = GetDegrees();
     //Serial.println(currentHeading);
     //vTaskDelay(100 / portTICK_PERIOD_MS); // Schedule to run every 100ms
@@ -256,7 +404,7 @@ void TaskControl(void *pvParameters) {
       isDrivingStraight = true;
     } else if(!isAvoidingObstacle) { // Try to go around obstacle.
       //Serial.println("Setup avoidance");
-      SimpleGyroNavigation();  // Pull sensors
+      //SimpleGyroNavigation();  // Pull sensors
       int16_t currentHeading = GetDegrees();      
       // todo: might want to back up too
       Motors(-100, -100);
@@ -291,6 +439,35 @@ void TaskControl(void *pvParameters) {
     }
     
     vTaskDelay(250 / portTICK_PERIOD_MS); // Schedule to run every 250ms, may need to lower this if there is a noticable pause in between switching the other tasks
+   }
+}
+
+// task code
+void TaskGyro(void *pvParameters) {
+  (void) pvParameters;
+   // Task setup here (like set a pin mode)
+   // Task loop here
+   while(1) { /* begin task loop */
+    
+    uint8_t xhi, xlo, ylo, yhi, zlo, zhi;
+    uint8_t out[6];
+    float gyroValue = 0;
+    //while(xSemaphoreTake(gyroSemaphore, (TickType_t) 10) == pdFALSE) {} // Wait on semaphore
+    I2C_read(&gyro, (L3GD20_REGISTER_OUT_X_L | 0x80), &out[0], 6); // read six bytes for position data
+    zlo = out[4];
+    zhi = out[5];
+    gyroValue = (int16_t)(zlo | (zhi << 8)) * L3GD20_SENSITIVITY_250DPS; // scale the value
+    if(abs(gyroValue) >= 1.0) { // noise floor
+      gyroAngle += gyroValue / 100.0; // integrate the value
+    }
+      gyroLoopCount++;
+      if(gyroLoopCount > 130) { // Handle drift, not perfect, but better than nothing
+        gyroLoopCount = 0;
+        gyroAngle -= 0.01;        
+      } 
+    //xSemaphoreGive(gyroSemaphore); // Release the semaphore 
+    
+    vTaskDelay(10 / portTICK_PERIOD_MS); // Schedule to run every 250ms, may need to lower this if there is a noticable pause in between switching the other tasks
    }
 }
 
